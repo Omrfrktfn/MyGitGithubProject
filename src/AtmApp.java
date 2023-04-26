dc635031c8babcc78bd7e79bb023344627b2737e
Omer <tufanfaruk71@gmail.com>
2023-04-26 11:02:14 +0200

    version2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -3,5 +3,7 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesi");
         //version1
+        System.out.println("java is easy");
+        //version2
     }
 }
